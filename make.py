26b724278325f1a55bcb28b54bae0fd1af5c1278
manatlan <manatlan@gmail.com>
2023-11-17 10:10:12 +0100

    better make.py

--- make.py
+++ make.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-import subprocess,sys,os,glob,re,json,statistics,re,fnmatch,time
+import subprocess,sys,os,glob,re,json,statistics,re,fnmatch,time,shutil,platform,multiprocessing
 """
 See doc :
 https://github.com/manatlan/sudoku_resolver/blob/master/make.md
@@ -28,6 +28,24 @@ LANGS=dict(
     	c="$0 $1",
         ext="js",
     ),
+    py3=dict(	
+        e="python3",
+    	c="$0 -uOO $1",
+        ext="py",
+    ),
+    rust=dict(	
+        e="rustc",
+    	c="$0 -C opt-level=3 -C target-cpu=native $1 -o exe && ./exe",
+        ext="rs",
+    ),
+    gcc=dict(	
+        e="gcc",
+    	c="$0 $1 -o exe && ./exe",
+        ext="c",
+    ),
+
+
+    #specifics ......................................................
     codon=dict(	
         e="~/.codon/bin/codon",
     	c="$0 run -release $1",
@@ -43,21 +61,7 @@ LANGS=dict(
     	c="$0 -uOO $1",
         ext="py",
     ),
-    py311=dict(	
-        e="python3.11",
-    	c="$0 -uOO $1",
-        ext="py",
-    ),
-    rust=dict(	
-        e="rustc",
-    	c="$0 -C opt-level=3 -C target-cpu=native $1 -o exe && ./exe",
-        ext="rs",
-    ),
-    gcc=dict(	
-        e="gcc",
-    	c="$0 $1 -o exe && ./exe",
-        ext="c",
-    ),
+
 )
 #########################################################################
 ## helpers
@@ -68,12 +72,29 @@ def myprint(*a,**k):
     k["flush"]=True
     print(*a,**k)
 
+todict = lambda x: dict( [[i.strip() for i in line.split(":",1) if ":" in line] for line in x.splitlines() if line.strip()] )
+
+def get_info_host() -> str:
+    s=f"PLATFORM : {platform.processor()}/{platform.platform()} with {multiprocessing.cpu_count()} cpus"
+    cp=subprocess.run(["cat","/proc/cpuinfo"],text=True,capture_output=True)
+    if cp.returncode==0:
+        d=todict(cp.stdout)
+        s+=f"""\nCPUINFO  : {d['vendor_id']} "{d['model name']}" ({d['bogomips']} bogomips)"""
+    cp=subprocess.run(["cat","/proc/meminfo"],text=True,capture_output=True)
+    if cp.returncode==0:
+        d=todict(cp.stdout)
+        s+=f"""\nMEMINFO  : {d['MemTotal']}"""
+    return s
+
 def update():
     """ update the global dict LANGS, to current supported lang of the host"""
     for k,v in list(LANGS.items()):
-        cp=subprocess.run(f"which {v['e']}",shell=True,text=True,capture_output=True)
-        if cp.returncode==0:
-            LANGS[k]['e']=cp.stdout.strip()
+        if os.path.isfile(os.path.expanduser(v['e'])):
+            cmd=os.path.expanduser(v['e'])
+        else:
+            cmd=shutil.which(v['e'])
+        if cmd:
+            LANGS[k]['e']=cmd.strip()
             cp=subprocess.run([LANGS[k]['e'],"--version"],text=True,capture_output=True)
             LANGS[k]['v']=cp.stdout.splitlines()[0]
         else:
@@ -82,7 +103,10 @@ def update():
 
 def help():
     print(f"USAGE TEST: {os.path.relpath(__file__)} <file|folder> ... <option>")
-    print(f"USAGE STAT: {os.path.relpath(__file__)} stats <file|folder> ...")
+    print(f"USAGE STAT: {os.path.relpath(__file__)} stats <file|folder> ... <option>")
+    print("Tool to test and sort results from differents interpreters/languages")
+    print("On the host:")
+    print(get_info_host())
     print()
     print("Where <option> can be, to force a specific one:")
     for k,v in LANGS.items():
@@ -174,12 +198,11 @@ def getinfo(file:str) -> str:
             return i[6:].strip()
     return "?"
 
-def stats(files:list):
+def stats(files:list, opts:list):
     for file in files:
         folder,filename = os.path.dirname(file) or ".",os.path.basename(file)
         results = sorted(glob.glob(f"{folder}/.outputs/{filename}|*"))
         if results:
-            myprint("\n",file,":",getinfo(file))
             
             bymode={}
             for result in results:
@@ -188,8 +211,12 @@ def stats(files:list):
                 data=json.load( open(result,"r+") )
                 seconds=getseconds(data["output"])
                 bymode.setdefault(mode,[]).append(seconds)
-            
+
+            if opts and (mode not in opts): continue
+            myprint("\n",file,":",getinfo(file))
+
             for mode, tests in bymode.items():
+                if opts and (mode not in opts): continue
                 moy= rr( statistics.median(tests) )
                 myprint(f"  - {mode:5s} : {moy} seconds ({len(tests)}x, {rr(min(tests)):.02f}><{rr(max(tests)):.02f})")
 
@@ -202,7 +229,10 @@ if __name__=="__main__":
         if args[0]=="stats":
             mode="stats"
             args.pop(0)
-            if not args: args=["."]
+            if not [i for i in args if not i.startswith("--")]:
+                # not files given in input, assuming '.'
+                args.insert(0,".")
+
         elif re.match("(\d+)x",args[0]):
             nb=int(re.match("(\d+)x",args[0])[1])
             mode="test"
@@ -236,7 +266,7 @@ if __name__=="__main__":
                 ret=batch(files, opts )
             myprint(f"(total time: %ss)" % rr(time.monotonic()-t))
         else:
-            ret=stats(files)
+            ret=stats(files, opts)
     else:
         ret=run("?","?")   # just for help prints 
     sys.exit(ret)
