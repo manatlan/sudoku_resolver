db1e694715ea4fc27a0e97176707c13841b02e0f
raffimolero <49224759+raffimolero@users.noreply.github.com>
2023-11-25 22:15:03 +0800

    30% faster
    i did not need to do this to myself

--- optimized/sudoku_specialized.rs
+++ optimized/sudoku_specialized.rs
@@ -5,9 +5,11 @@
 
 use std::{fmt::Display, fs, ops::SubAssign, str::FromStr};
 
+/// A Set intended to contain every valid number an empty tile could be
 struct NumSet([bool; 9]);
 
 impl NumSet {
+    /// creates a new set where any number is possible
     fn all() -> Self {
         Self([true; 9])
     }
@@ -19,6 +21,7 @@ impl NumSet {
             .filter_map(|(i, v)| v.then(|| i as u8))
     }
 
+    /// returns how many possibilities there are for this tile
     fn len(&self) -> u8 {
         self.0.iter().filter(|n| **n).count() as u8
     }
@@ -27,7 +30,7 @@ impl NumSet {
 impl<T: IntoIterator<Item = u8>> SubAssign<T> for NumSet {
     fn sub_assign(&mut self, rhs: T) {
         for n in rhs {
-            if n == EMPTY {
+            if n == Grid::EMPTY {
                 continue;
             }
             self.0[n as usize] = false;
@@ -35,6 +38,68 @@ impl<T: IntoIterator<Item = u8>> SubAssign<T> for NumSet {
     }
 }
 
+/// A Set intended to contain every hole inside the sudoku board.
+struct SpaceSet {
+    data: [usize; 81],
+    len: usize,
+}
+
+impl FromIterator<usize> for SpaceSet {
+    fn from_iter<T: IntoIterator<Item = usize>>(iter: T) -> Self {
+        let mut out = Self::empty();
+        for n in iter {
+            out.insert(n);
+        }
+        out
+    }
+}
+
+impl SpaceSet {
+    fn empty() -> Self {
+        Self {
+            data: [0; 81],
+            len: 0,
+        }
+    }
+
+    fn insert(&mut self, item: usize) {
+        self.data[self.len as usize] = item;
+        self.len += 1;
+    }
+
+    fn remove(&mut self, index: usize) {
+        self.len -= 1;
+        self.data[index] = self.data[self.len];
+    }
+
+    fn iter<'a>(&'a self) -> impl 'a + Iterator<Item = usize> {
+        self.data[..self.len as usize].iter().copied()
+    }
+
+    /// creates a new SpaceSet to track all the holes in the grid
+    fn find_all(grid: &Grid) -> Self {
+        grid.data
+            .iter()
+            .enumerate()
+            .filter_map(|(i, n)| (*n == Grid::EMPTY).then_some(i))
+            .collect()
+    }
+
+    // NOTE: this is quite heavily coupled to the implementation details of Grid.
+    // i could make it as generic as Iterator::min_by_key, but it is not necessary.
+    /// finds a tile in the grid with the fewest possible outcomes
+    fn pop_min(&mut self, grid: &Grid) -> Option<(usize, NumSet)> {
+        let (i, out) = self
+            .iter()
+            .map(|space| (space, grid.free(space % 9, space / 9)))
+            .enumerate()
+            .min_by_key(|(_i, (_space, num_set))| num_set.len())?;
+        self.remove(i);
+        Some(out)
+    }
+}
+
+/// a sudoku grid where empty tiles are 255 and numbers range from 0..=8
 struct Grid {
     data: [u8; 81],
 }
@@ -46,8 +111,8 @@ impl FromStr for Grid {
         let mut data = <[u8; 81]>::try_from(s.as_bytes()).map_err(|_| ())?;
         for c in data.iter_mut() {
             *c = match c {
-                b'.' => EMPTY,
-                _ => *c - ZERO_IDX,
+                b'.' => Grid::EMPTY,
+                _ => *c - Grid::ZERO_IDX,
             };
         }
         Ok(Self { data })
@@ -58,8 +123,8 @@ impl Display for Grid {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         for &n in &self.data {
             let c = match n {
-                EMPTY => b'.',
-                _ => n + ZERO_IDX,
+                Grid::EMPTY => b'.',
+                _ => n + Grid::ZERO_IDX,
             } as char;
             write!(f, "{c}")?;
         }
@@ -67,10 +132,11 @@ impl Display for Grid {
     }
 }
 
-const EMPTY: u8 = 255;
-const ZERO_IDX: u8 = b'1';
-
 impl Grid {
+    const EMPTY: u8 = 255;
+    const ZERO_IDX: u8 = b'1';
+
+    /// returns the values of all tiles in the block that a tile belongs to
     fn sqr<'a>(&'a self, x: usize, y: usize) -> impl 'a + Iterator<Item = u8> {
         let x = (x / 3) * 3;
         let y = (y / 3) * 3;
@@ -82,14 +148,17 @@ impl Grid {
             .copied()
     }
 
+    /// returns the values of all tiles in the column that a tile belongs to
     fn col<'a>(&'a self, x: usize) -> impl 'a + Iterator<Item = u8> {
         (0..9).map(move |y| self.data[x + y * 9])
     }
 
+    /// returns the values of all tiles in the row that a tile belongs to
     fn row<'a>(&'a self, y: usize) -> impl 'a + Iterator<Item = u8> {
         self.data[y * 9..y * 9 + 9].iter().copied()
     }
 
+    /// finds all the possible values that a given tile can be
     fn free(&self, x: usize, y: usize) -> NumSet {
         let mut all_chars: NumSet = NumSet::all();
         all_chars -= self.row(y);
@@ -98,30 +167,32 @@ impl Grid {
         all_chars
     }
 
-    fn resolv_inner(&mut self, pos: usize) -> bool {
-        let Some((i, set)) = self.data[pos..]
-            .iter()
-            .enumerate()
-            .filter_map(|(i, tile)| (*tile == EMPTY).then(|| (i, self.free(i % 9, i / 9))))
-            .min_by_key(|(_i, num_set)| num_set.len())
-        else {
+    fn resolve_inner(&mut self, spaces: &mut SpaceSet) -> bool {
+        let Some((i, set)) = spaces.pop_min(&self) else {
             return true;
         };
-
         for elem in set.iter() {
             self.data[i] = elem;
-            if self.resolv_inner(pos.min(i)) {
+            if self.resolve_inner(spaces) {
                 return true;
             }
-            self.data[i] = EMPTY;
+            self.data[i] = Grid::EMPTY;
         }
+        spaces.insert(i);
         false
+    }
+
+    /// attempts to solve the board through mutation, and returns true if it is solved
+    /// returns false if it is unsolvable, and i believe it returns to its original state as well
+    fn resolve(&mut self) -> bool {
+        let mut spaces = SpaceSet::find_all(&self);
+        self.resolve_inner(&mut spaces)
     }
 }
 
-fn resolv(g: &str) -> Option<String> {
+fn resolve_str(g: &str) -> Option<String> {
     let mut g = g.parse::<Grid>().unwrap();
-    g.resolv_inner(0).then(|| g.to_string())
+    g.resolve().then(|| g.to_string())
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -130,7 +201,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let t = std::time::Instant::now();
     for g in gg {
-        if let Some(rg) = resolv(g) {
+        if let Some(rg) = resolve_str(g) {
             if rg.chars().any(|c| c == '.') {
                 panic!("not resolved ?!");
             }
