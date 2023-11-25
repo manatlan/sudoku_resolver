103a2a91111065b46f903f4abdd5a3ba3350c6f5
manatlan <manatlan@gmail.com>
2023-11-25 13:48:47 +0100

    optim from redstoneboi (to avoid skip/take)

--- sudoku.rs
+++ sudoku.rs
@@ -1,24 +1,29 @@
 #!./make.py
 //INFO: the simple algo, with strings (AI translation from java one) (100grids)
-
-use std::fs;
-use std::collections::HashSet;
+use std::{collections::HashSet, fs};
 
 fn sqr(g: &str, x: usize, y: usize) -> String {
     let x = (x / 3) * 3;
     let y = (y / 3) * 3;
-    g.chars().skip(y * 9 + x).take(3)
-        .chain(g.chars().skip(y * 9 + x + 9).take(3))
-        .chain(g.chars().skip(y * 9 + x + 18).take(3))
+    let i = y * 9 + x;
+    g[i..i + 3]
+        .chars()
+        .chain(g[i + 9..i + 9 + 3].chars())
+        .chain(g[i + 18..i + 18 + 3].chars())
         .collect()
 }
 
 fn col(g: &str, x: usize) -> String {
-    (0..9).map(|y| g.chars().skip(x + y * 9).next().unwrap()).collect()
+    (0..9)
+        .map(move |y| {
+            let i = x + y * 9;
+            g[i..i + 1].chars().next().unwrap()
+        })
+        .collect()
 }
 
 fn row(g: &str, y: usize) -> String {
-    g.chars().skip(y * 9).take(9).collect()
+    g[y * 9..y * 9 + 9].chars().collect()
 }
 
 fn freeset(g: &str) -> HashSet<char> {
@@ -38,10 +43,10 @@ fn free(g: &str, x: usize, y: usize) -> HashSet<char> {
     freeset(&all_chars.iter().collect::<String>())
 }
 
-fn resolv(g: &str) -> Option<String> {
+fn resolv(g: String) -> Option<String> {
     if let Some(i) = g.find('.') {
-        for elem in free(g, i % 9, i / 9) {
-            if let Some(ng) = resolv(&format!("{}{}{}", &g[..i], elem, &g[i+1..])) {
+        for elem in free(&g, i % 9, i / 9) {
+            if let Some(ng) = resolv(format!("{}{}{}", &g[..i], elem, &g[i + 1..])) {
                 return Some(ng);
             }
         }
@@ -50,20 +55,19 @@ fn resolv(g: &str) -> Option<String> {
         Some(g.to_string())
     }
 }
-
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let content = fs::read_to_string("grids.txt")?;
     let gg: Vec<&str> = content.lines().take(100).collect();
 
     let t = std::time::Instant::now();
     for g in gg {
-        if let Some(rg) = resolv(g) {
+        if let Some(rg) = resolv(g.to_owned()) {
             if rg.chars().any(|c| c == '.') {
                 panic!("not resolved ?!");
             }
             println!("{}", rg);
         }
     }
-    println!("Took: {} s", (t.elapsed().as_millis() as f32)/1000.0);
+    println!("Took: {} s", (t.elapsed().as_millis() as f32) / 1000.0);
     Ok(())
 }
