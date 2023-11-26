ff13f24cfc9b7be064c60a57a2cc45de689bbcb1
raffimolero <49224759+raffimolero@users.noreply.github.com>
2023-11-26 18:52:03 +0800

    Optimize sudoku_specialized.rs 2x by directly storing CandidateSets in the grid
    Actually found a new optimization, 2x on my macbook air

--- optimized/sudoku_specialized.rs
+++ optimized/sudoku_specialized.rs
@@ -3,104 +3,127 @@
 
 //INFO: the optimized algo, with ultra-specialized types/api (1956grids)
 
-use std::fs;
-use std::fmt::Formatter;
-use std::str::FromStr;
-use std::fmt::Display;
-use std::ops::AddAssign;
-use std::ops::SubAssign;
-use std::ops::Sub;
-use std::ops::Add;
+use std::{
+    fmt::{Display, Formatter},
+    fs,
+    ops::{Add, AddAssign, Sub, SubAssign},
+    str::FromStr,
+};
 
-#[derive(Clone,Eq,PartialEq)]
-struct Grid([Option<u8>;81]);
+#[derive(Clone, Eq, PartialEq)]
+struct Grid([CandidateSet; 81]);
 
-#[derive(Copy,Clone,Eq,PartialEq)]
+#[derive(Copy, Clone, Eq, PartialEq)]
 struct CandidateSet(u16);
 
 impl CandidateSet {
-    fn all()->Self { CandidateSet(0x1ff) }
-    fn empty()->Self { CandidateSet(0) }
-    fn singleton(val:u8)->Self { CandidateSet(1 << val) }
-    fn from_iter(it: impl Iterator<Item=u8>)->Self {
-        let mut ret = Self::empty();
-        for i in it {
-            ret += Self::singleton(i);
-        }
-        ret
+    const ALL: Self = Self(0b_111_111_111);
+    const EMPTY: Self = Self(0);
+
+    fn one_hot(val: u8) -> Self {
+        CandidateSet(1 << val)
     }
-    fn len(self)->u32 { self.0.count_ones() }
+
+    fn val(self) -> u8 {
+        self.0.trailing_zeros() as u8
+    }
+
+    fn from_iter(it: impl IntoIterator<Item = Self>) -> Self {
+        it.into_iter()
+            .reduce(|a, b| a + b)
+            .expect("iterator must have more than 1 value")
+    }
+
+    fn len(self) -> u32 {
+        self.0.count_ones()
+    }
 }
 
 impl Add for CandidateSet {
     type Output = Self;
-    fn add(self, rhs: Self)->Self { CandidateSet(self.0 | rhs.0) }
+    fn add(self, rhs: Self) -> Self {
+        CandidateSet(self.0 | rhs.0)
+    }
 }
 
 impl AddAssign for CandidateSet {
-    fn add_assign(&mut self, rhs: Self) { *self = *self + rhs; }
+    fn add_assign(&mut self, rhs: Self) {
+        *self = *self + rhs;
+    }
 }
 
 impl Sub for CandidateSet {
     type Output = Self;
-    fn sub(self, rhs: Self)->Self { CandidateSet(self.0 & !rhs.0) }
+    fn sub(self, rhs: Self) -> Self {
+        CandidateSet(self.0 & !rhs.0)
+    }
 }
 
 impl SubAssign for CandidateSet {
-    fn sub_assign(&mut self, rhs: Self) { *self = *self - rhs; }
+    fn sub_assign(&mut self, rhs: Self) {
+        *self = *self - rhs;
+    }
 }
 
 struct CandidateIter {
     set: CandidateSet,
-    pos: u8
+    mask: u16,
 }
 
 impl Iterator for CandidateIter {
-    type Item = u8;
-    fn next(&mut self)->Option<u8> {
-        for i in self.pos .. 9 {
-            if (self.set.0 & (1 << i)) != 0 {
-                self.pos = i+1;
-                return Some(i)
+    type Item = CandidateSet;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        while self.mask <= (1 << 9) {
+            let masked = self.set.0 & self.mask;
+            self.mask <<= 1;
+            if masked != 0 {
+                return Some(CandidateSet(masked));
             }
         }
-        self.pos = 9;
         None
     }
 }
 
 impl IntoIterator for CandidateSet {
-    type Item = u8;
+    type Item = CandidateSet;
     type IntoIter = CandidateIter;
-    fn into_iter(self)->CandidateIter { CandidateIter { set: self, pos: 0 } }
+    fn into_iter(self) -> CandidateIter {
+        CandidateIter { set: self, mask: 1 }
+    }
 }
 
-
 impl Grid {
-    fn sqr(&self, x: usize, y:usize)->CandidateSet {
-        CandidateSet::from_iter(self.0[y*9+    x..y*9+x+3].iter().filter_map(|&opt| opt)) +
-        CandidateSet::from_iter(self.0[y*9+x+ 9..y*9+x+12].iter().filter_map(|&opt| opt)) +
-        CandidateSet::from_iter(self.0[y*9+x+18..y*9+x+21].iter().filter_map(|&opt| opt))
+    fn sqr(&self, x: usize, y: usize) -> CandidateSet {
+        let x = (x / 3) * 3;
+        let y = (y / 3) * 3;
+        let i = y * 9 + x;
+        CandidateSet::from_iter(self.0[i..i + 3].iter().copied())
+            + CandidateSet::from_iter(self.0[i + 9..i + 12].iter().copied())
+            + CandidateSet::from_iter(self.0[i + 18..i + 21].iter().copied())
     }
-    
-    fn col(&self, x:usize)->CandidateSet {
-        CandidateSet::from_iter((0..9).map(|y| &self.0[x+9*y]).filter_map(|&opt| opt))
+
+    fn col(&self, x: usize) -> CandidateSet {
+        CandidateSet::from_iter((0..9).map(|y| self.0[y * 9 + x]))
     }
-    
-    fn row(&self, y:usize)->CandidateSet {
-        CandidateSet::from_iter(self.0[y*9..y*9+9].iter().filter_map(|&opt| opt))
+
+    fn row(&self, y: usize) -> CandidateSet {
+        CandidateSet::from_iter(self.0[y * 9..(y + 1) * 9].iter().copied())
     }
-    
-    fn free(&self, x:usize, y:usize)->CandidateSet {
-        CandidateSet::all() - (self.col(x) + self.row(y) + self.sqr((x/3)*3, (y/3)*3))
+
+    fn free(&self, x: usize, y: usize) -> CandidateSet {
+        let col = self.col(x);
+        let row = self.row(y);
+        let sqr = self.sqr(x, y);
+        CandidateSet::ALL - (col + row + sqr)
     }
-    
-    fn resolv(&mut self)->bool {
+
+    fn resolv(&mut self) -> bool {
         let mut ibest = None;
-        let mut cbest = CandidateSet::all();
+        let mut cbest = CandidateSet::ALL;
         for i in 0..81 {
-            if self.0[i].is_none() {
-                let c = self.free(i%9, i/9);
+            if self.0[i] == CandidateSet::EMPTY {
+                let c = self.free(i % 9, i / 9);
                 if c.len() == 0 {
                     // unsolvable
                     return false;
@@ -115,17 +138,19 @@ impl Grid {
                 }
             }
         }
-        
+
         let i = match ibest {
             None => return true, // solved
-            Some(i) => i
+            Some(i) => i,
         };
-        
+
         for c in cbest {
-            self.0[i] = Some(c);
-            if self.resolv() { return true; }
+            self.0[i] = c;
+            if self.resolv() {
+                return true;
+            }
         }
-        self.0[i] = None;
+        self.0[i] = CandidateSet::EMPTY;
         false
     }
 }
@@ -139,10 +164,13 @@ impl FromStr for Grid {
     type Err = ParseGridError;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let mut grid = [None;81];
-        for (i,(g,c)) in grid.iter_mut().zip(s.chars()).enumerate() {
-            if ('1'..='9').contains(&c) { *g = Some(c as u8-'1' as u8); }
-            else if c != '.' { return Err(ParseGridError { pos: i }) }
+        let mut grid = [CandidateSet::EMPTY; 81];
+        for (i, (g, c)) in grid.iter_mut().zip(s.chars()).enumerate() {
+            if ('1'..='9').contains(&c) {
+                *g = CandidateSet::one_hot(c as u8 - b'1');
+            } else if c != '.' {
+                return Err(ParseGridError { pos: i });
+            }
         }
         Ok(Grid(grid))
     }
@@ -151,17 +179,20 @@ impl FromStr for Grid {
 impl Display for Grid {
     fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), std::fmt::Error> {
         for g in self.0 {
-            write!(f, "{}", match g {
-                Some(x) => ('1' as u8 + x) as char,
-                None => '.'
-            })?;
+            write!(
+                f,
+                "{}",
+                match g {
+                    CandidateSet::EMPTY => '.',
+                    _ => (b'1' + g.val()) as char,
+                }
+            )?;
         }
         Ok(())
     }
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-
     let content = fs::read_to_string("grids.txt")?;
     let gg: Vec<&str> = content.lines().take(1956).collect();
 
@@ -171,6 +202,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         grid.resolv();
         println!("{} ", grid);
     }
-    println!("Took: {} s", (t.elapsed().as_millis() as f32)/1000.0);
+    println!("Took: {} s", (t.elapsed().as_millis() as f32) / 1000.0);
     Ok(())
 }
