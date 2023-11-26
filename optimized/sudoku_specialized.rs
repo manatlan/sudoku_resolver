d8f39c1b33b65166f57ac03b229964cc2151af85
raffimolero <49224759+raffimolero@users.noreply.github.com>
2023-11-26 19:23:46 +0800

    from 1.8s to 1.1s on my mac
    added a new struct to track all the empty spaces in the grid

--- optimized/sudoku_specialized.rs
+++ optimized/sudoku_specialized.rs
@@ -11,7 +11,10 @@ use std::{
 };
 
 #[derive(Clone, Eq, PartialEq)]
-struct Grid([CandidateSet; 81]);
+struct Grid {
+    data: [CandidateSet; 81],
+    spaces: SpaceSet,
+}
 
 #[derive(Copy, Clone, Eq, PartialEq)]
 struct CandidateSet(u16);
@@ -74,7 +77,7 @@ impl Iterator for CandidateIter {
     type Item = CandidateSet;
 
     fn next(&mut self) -> Option<Self::Item> {
-        while self.mask <= (1 << 9) {
+        while self.mask < (1 << 9) {
             let masked = self.set.0 & self.mask;
             self.mask <<= 1;
             if masked != 0 {
@@ -93,22 +96,69 @@ impl IntoIterator for CandidateSet {
     }
 }
 
+#[derive(Clone, Eq, PartialEq)]
+struct SpaceSet {
+    data: [usize; 81],
+    len: usize,
+}
+
+impl FromIterator<usize> for SpaceSet {
+    fn from_iter<T: IntoIterator<Item = usize>>(iter: T) -> Self {
+        let mut out = Self::empty();
+        for n in iter {
+            out.insert(n);
+        }
+        out
+    }
+}
+
+impl SpaceSet {
+    fn empty() -> Self {
+        Self {
+            data: [0; 81],
+            len: 0,
+        }
+    }
+
+    fn insert(&mut self, item: usize) {
+        self.data[self.len] = item;
+        self.len += 1;
+    }
+
+    fn remove(&mut self, index: usize) {
+        self.len -= 1;
+        self.data[index] = self.data[self.len];
+    }
+
+    fn iter(&self) -> impl '_ + Iterator<Item = usize> {
+        self.data[..self.len].iter().copied()
+    }
+
+    /// creates a new SpaceSet to track all the holes in the grid
+    fn find_all(data: &[CandidateSet; 81]) -> Self {
+        data.iter()
+            .enumerate()
+            .filter_map(|(i, n)| (*n == CandidateSet::EMPTY).then_some(i))
+            .collect()
+    }
+}
+
 impl Grid {
     fn sqr(&self, x: usize, y: usize) -> CandidateSet {
         let x = (x / 3) * 3;
         let y = (y / 3) * 3;
         let i = y * 9 + x;
-        CandidateSet::from_iter(self.0[i..i + 3].iter().copied())
-            + CandidateSet::from_iter(self.0[i + 9..i + 12].iter().copied())
-            + CandidateSet::from_iter(self.0[i + 18..i + 21].iter().copied())
+        CandidateSet::from_iter(self.data[i..i + 3].iter().copied())
+            + CandidateSet::from_iter(self.data[i + 9..i + 12].iter().copied())
+            + CandidateSet::from_iter(self.data[i + 18..i + 21].iter().copied())
     }
 
     fn col(&self, x: usize) -> CandidateSet {
-        CandidateSet::from_iter((0..9).map(|y| self.0[y * 9 + x]))
+        CandidateSet::from_iter((0..9).map(|y| self.data[y * 9 + x]))
     }
 
     fn row(&self, y: usize) -> CandidateSet {
-        CandidateSet::from_iter(self.0[y * 9..(y + 1) * 9].iter().copied())
+        CandidateSet::from_iter(self.data[y * 9..(y + 1) * 9].iter().copied())
     }
 
     fn free(&self, x: usize, y: usize) -> CandidateSet {
@@ -121,36 +171,37 @@ impl Grid {
     fn resolv(&mut self) -> bool {
         let mut ibest = None;
         let mut cbest = CandidateSet::ALL;
-        for i in 0..81 {
-            if self.0[i] == CandidateSet::EMPTY {
-                let c = self.free(i % 9, i / 9);
-                if c.len() == 0 {
-                    // unsolvable
-                    return false;
-                }
-                if c.len() < cbest.len() {
-                    ibest = Some(i);
-                    cbest = c;
-                }
-                if c.len() == 1 {
-                    // Only one candidate here; we can't do better...
-                    break;
-                }
+        for (i, s) in self.spaces.iter().enumerate() {
+            let c = self.free(s % 9, s / 9);
+            if c.len() == 0 {
+                // unsolvable
+                return false;
+            }
+            if c.len() < cbest.len() {
+                ibest = Some((i, s));
+                cbest = c;
+            }
+            if c.len() == 1 {
+                // Only one candidate here; we can't do better...
+                break;
             }
         }
 
-        let i = match ibest {
-            None => return true, // solved
-            Some(i) => i,
+        let Some((i, s)) = ibest else {
+            // solved
+            return true;
         };
+        self.spaces.remove(i);
 
         for c in cbest {
-            self.0[i] = c;
+            self.data[s] = c;
             if self.resolv() {
                 return true;
             }
         }
-        self.0[i] = CandidateSet::EMPTY;
+
+        self.spaces.insert(s);
+        self.data[s] = CandidateSet::EMPTY;
         false
     }
 }
@@ -164,21 +215,22 @@ impl FromStr for Grid {
     type Err = ParseGridError;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let mut grid = [CandidateSet::EMPTY; 81];
-        for (i, (g, c)) in grid.iter_mut().zip(s.chars()).enumerate() {
+        let mut data = [CandidateSet::EMPTY; 81];
+        for (i, (g, c)) in data.iter_mut().zip(s.chars()).enumerate() {
             if ('1'..='9').contains(&c) {
                 *g = CandidateSet::one_hot(c as u8 - b'1');
             } else if c != '.' {
                 return Err(ParseGridError { pos: i });
             }
         }
-        Ok(Grid(grid))
+        let spaces = SpaceSet::find_all(&data);
+        Ok(Grid { data, spaces })
     }
 }
 
 impl Display for Grid {
     fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), std::fmt::Error> {
-        for g in self.0 {
+        for g in self.data {
             write!(
                 f,
                 "{}",
