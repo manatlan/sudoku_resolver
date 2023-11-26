c8ce79e172e3d8fa152e3d6a15895c6da8c4bf7f
Samuel S. Shepard <sammysheep@gmail.com>
2023-11-26 14:40:35 -0500

    Remove need to HashSet similar to the C version.

--- sudoku.rs
+++ sudoku.rs
@@ -3,15 +3,15 @@
 
 // This version doesn't use "Strings", but "byte[]", because :
 // The weapons are not the same because other languages do not have UTF-8 encoded
-// strings. So Rust is at huge disadvantage here for no reason. String operations 
-// that are constant time in other languages take linear time in Rust because it 
+// strings. So Rust is at huge disadvantage here for no reason. String operations
+// that are constant time in other languages take linear time in Rust because it
 // tries to handle the complexity of UTF correctly. (https://www.reddit.com/user/ondrejdanek/)
 // https://www.reddit.com/r/rust/comments/183ex3i/comment/kapmmub/?utm_source=share&utm_medium=web2x&context=3
 
 // the original with string is here "./experiments/sudoku_original_with_strings_only.rs" (~30s)
 // thanks @noamtashma for the fix !
 
-use std::{collections::HashSet, fs};
+use std::fs;
 
 fn sqr(g: &[u8], x: usize, y: usize) -> impl Iterator<Item = u8> + '_ {
     let x = (x / 3) * 3;
@@ -33,15 +33,20 @@ fn row(g: &[u8], y: usize) -> impl Iterator<Item = u8> + '_ {
     g[y * 9..y * 9 + 9].iter().copied()
 }
 
-fn freeset(g: impl Iterator<Item = u8>) -> HashSet<u8> {
-    let mut all_digits: HashSet<u8> = b"123456789".iter().copied().collect();
+fn freeset(g: impl Iterator<Item = u8>) -> [bool; 9] {
+    // This is closer to the C version, but a bitset might be good to try as
+    // well. Hashing over a small dataset requires an allocation and the
+    // constant lookup for hashing only matters when the array N gets large.
+    let mut all_digits = [true; 9];
     for digit in g {
-        all_digits.remove(&digit);
+        if (b'1'..=b'9').contains(&digit) {
+            all_digits[(digit - b'1') as usize] = false;
+        }
     }
     all_digits
 }
 
-fn free(g: &[u8], x: usize, y: usize) -> HashSet<u8> {
+fn free(g: &[u8], x: usize, y: usize) -> [bool; 9] {
     let row_bytes = row(g, y);
     let col_bytes = col(g, x);
     let sqr_bytes = sqr(g, x, y);
@@ -51,11 +56,13 @@ fn free(g: &[u8], x: usize, y: usize) -> HashSet<u8> {
 
 fn resolv(g: &[u8]) -> Option<Vec<u8>> {
     if let Some(i) = g.iter().position(|&c| c == b'.') {
-        for elem in free(g, i % 9, i / 9) {
-            let mut new_board = g.to_owned();
-            new_board[i] = elem;
-            if let Some(ng) = resolv(&new_board) {
-                return Some(ng);
+        for (k, free) in free(g, i % 9, i / 9).iter().copied().enumerate() {
+            if free {
+                let mut new_board = g.to_owned();
+                new_board[i] = k as u8 + b'1';
+                if let Some(ng) = resolv(&new_board) {
+                    return Some(ng);
+                }
             }
         }
         None
