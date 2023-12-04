466e9ea2a82d5d1a85af2a80e575c7088ae03d29
manatlan <manatlan@gmail.com>
2023-12-04 08:52:30 +0100

    Create _sudoku.go

---
+++ _sudoku.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+// sqr returns the square of the Sudoku grid at the specified coordinates.
+func sqr(g string, x, y int) string {
+	x = (x / 3) * 3
+	y = (y / 3) * 3
+	return g[y*9+x:y*9+x+3] + g[y*9+x+9:y*9+x+12] + g[y*9+x+18:y*9+x+21]
+}
+
+// col returns the column of the Sudoku grid at the specified column index.
+func col(g string, x int) string {
+	var result strings.Builder
+	for y := 0; y < 9; y++ {
+		ligne := y * 9
+		result.WriteByte(g[x+ligne])
+	}
+	return result.String()
+}
+
+// row returns the row of the Sudoku grid at the specified row index.
+func row(g string, y int) string {
+	ligne := y * 9
+	return g[ligne : ligne+9]
+}
+
+// free returns the set of free numbers at the specified coordinates in the Sudoku grid.
+func free(g string, x, y int) string {
+	all := "123456789"
+	t27 := row(g, y) + col(g, x) + sqr(g, x, y)
+	var freeset strings.Builder
+
+	for i := 0; i < 9; i++ {
+		c := all[i]
+		if strings.IndexRune(t27, c) < 0 {
+			freeset.WriteByte(c)
+		}
+	}
+	return freeset.String()
+}
+
+// resolv solves the Sudoku grid using backtracking.
+func resolv(g string) string {
+	ibest := -1
+	cbest := "123456789"
+
+	for i := 0; i < 81; i++ {
+		if g[i] == '.' {
+			c := free(g, i%9, i/9)
+			if len(c) == 0 {
+				return ""
+			}
+			if len(c) < len(cbest) {
+				ibest = i
+				cbest = c
+			}
+			if len(c) == 1 {
+				break
+			}
+		}
+	}
+
+	if ibest >= 0 {
+		for j := 0; j < len(cbest); j++ {
+			elem := cbest[j]
+			ng := resolv(g[:ibest] + string(elem) + g[ibest+1:])
+			if ng != "" {
+				return ng
+			}
+		}
+		return ""
+	} else {
+		return g
+	}
+}
+
+func main() {
+	// Uncomment the following lines to read grids from a file
+	// content, err := os.ReadFile("grids.txt")
+	// if err != nil {
+	// 	fmt.Println("Error reading file:", err)
+	// 	return
+	// }
+	// grids := strings.Split(string(content), "\n")
+
+	// Uncomment the following line to read grids from standard input
+	// scanner := bufio.NewScanner(os.Stdin)
+
+	// for _, g := range grids {
+	// 	fmt.Println(resolv(g))
+	// }
+
+	// Uncomment the following lines to read grids from standard input
+	// for scanner.Scan() {
+	// 	fmt.Println(resolv(scanner.Text()))
+	// }
+
+	// Uncomment the following line to read a single grid from standard input
+	var input string
+	for {
+		_, err := fmt.Scanln(&input)
+		if err != nil {
+			break
+		}
+		fmt.Println(resolv(input))
+	}
+}
